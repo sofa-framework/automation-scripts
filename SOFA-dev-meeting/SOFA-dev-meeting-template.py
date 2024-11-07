457b8e49f7b486bdfa66439326cd3ba0dadccf99
hugtalbot <hugo.talbot@sofa-framework.org>
2024-11-07 23:31:49 +0100

    SOFA-dev-meeting: post as a file (2)

--- SOFA-dev-meeting/SOFA-dev-meeting-template.py
+++ SOFA-dev-meeting/SOFA-dev-meeting-template.py
@@ -9,7 +9,8 @@ from list_topics_to_be_discussed import list_topics_to_be_discussed
 discord_token = os.environ['DISCORD_WEBHOOK_URL']
 
 def postOnDiscord():
-    response = requests.post(discord_token, files={"file": open("SOFA-dev-meeting.md", "rb")})
+    files = {'upload_file': open('SOFA-dev-meeting.md','rb')}
+    response = requests.post(discord_token, files)
     print("Status: "+str(response.status_code)+"\nReason: "+str(response.reason)+"\nText: "+str(response.text))
     return
 
@@ -22,17 +23,17 @@ def main(argv):
     today = date.today()
     dateToday = today.strftime("%d/%m/%Y")
     
-    message="# "+str(dateToday)+"\n### News\n- \n\n### Technical discussions"
+    message="# "+str(dateToday)+"\n### News\n- \n\n### Technical discussions\n"
 
     output = list_topics_to_be_discussed("sofa")
-    message = message + output
+    message = message + output + "\n"
 
-    message = message + "### PR review"
+    message = message + "### PR review\n"
     for arg in argv[1:]:
         output = list_to_review_pr(arg)
         message = message + output +"\n"
 
-    message = message + "### PR merged within the week"
+    message = message + "### PR merged within the week\n"
     for arg in argv[1:]:
         output = list_merged_pr(arg)
         message = message + output +"\n"
